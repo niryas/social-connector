ea77ba90114e20b92b54089e20a54d36dddf0ddc
niryasoor <niryas@gmail.com>
2024-03-14 17:07:21 +0200

    test: built library type is correct

--- test/Lib.test.ts
+++ test/Lib.test.ts
@@ -1,8 +1,13 @@
 import { describe, test, expect } from "vitest";
 import { Instagram } from "../";
+import IG from "../src/Instagram";
 
 describe("Built library", () => {
 	test("Instagram class is a named import", () => {
 		expect(Instagram).toBeDefined();
+	});
+
+	test("Instagram import is of type Instagram", () => {
+		expect(Instagram).toBeTypeOf(typeof IG);
 	});
 });
